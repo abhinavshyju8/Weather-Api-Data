16ae5c5f5e14b495de7255f7061c51eb7e30521d
abhinavshyju8 <abhinavshyju8@gmail.com>
2026-05-14 20:47:24 +0530

    Configured Snowflake external stage for S3 weather data integration

--- Snowflake.sql
+++ Snowflake.sql
@@ -1,2 +1,10 @@
 CREATE OR REPLACE FILE FORMAT weather_json
 TYPE = 'JSON';
+
+CREATE OR REPLACE STAGE my_stage
+URL = 's3://weather-bucket-1111/'
+CREDENTIALS = (
+    AWS_KEY_ID = os.getenv('AWS_KEY_ID'),
+    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+)
+FILE_FORMAT = weather_json;
