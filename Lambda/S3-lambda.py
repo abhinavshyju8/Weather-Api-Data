1fe5e9fa28d2edb94ad61c3823b8791b0002344a
abhinavshyju8 <abhinavshyju8@gmail.com>
2026-05-14 20:14:19 +0530

    Transformed DynamoDB stream data into structured JSON format

--- Lambda/S3-lambda.py
+++ Lambda/S3-lambda.py
@@ -7,3 +7,10 @@ BUCKET_NAME = "weather-bucket-1111"
 def lambda_handler(event, context):
     for record in event['Records']:
         new_image = record['dynamodb']['NewImage']       
+
+        item = {
+            "city": new_image['city']['S'],
+            "time": new_image['time']['S'],
+            "temperature": new_image['temperature']['S'],
+            "description": new_image['description']['S']
+        }
