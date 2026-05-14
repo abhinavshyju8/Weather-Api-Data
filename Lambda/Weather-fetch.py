2d14a60fe00f456f38faf071677ea68571380d7e
abhinavshyju8 <abhinavshyju8@gmail.com>
2026-05-14 19:52:49 +0530

    API endpoint for weather data retrieval

--- Lambda/Weather-fetch.py
+++ Lambda/Weather-fetch.py
@@ -7,3 +7,5 @@ def lambda_handler(event, context):
 
     api_key = "04c20c5de85f796888612a01e8d749d9"
     city = "Kozhikode"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
